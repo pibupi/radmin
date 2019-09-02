e3881d70f46893c777e239b0ec3854c284a3abc1
aicoder <malun666@126.com>
2019-09-02 19:26:05 +0800

    :art:added login home 404 pages and added config of router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Empty from './View/Empty';
+import Home from './View/Home';
+import Login from './View/Login';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h3>hello, admin</h3>
-      </header>
-    </div>
+   <Router>
+     <Switch>
+      <Route path="/" exact render={(props) => {
+        return <Redirect to="/home"></Redirect>
+      }}></Route>
+      <Route path="/home" render={(props) => {
+        return <Home {...props}></Home>
+      }} ></Route>
+      <Route path="/login" component={Login}></Route>
+      <Route component={Empty}></Route>
+     </Switch>
+   </Router>
   );
 }
 
---
+++ src/View/Home/index.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+          Home
+      </div>
+    )
+  }
+}
+
+Home.propTypes = {
+
+}
+
+export default Home
