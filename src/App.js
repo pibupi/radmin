09f918559ecd0e8289275ccf348af63a1d3eae63
aicoder <malun666@126.com>
2019-09-02 19:58:17 +0800

    :tada: added the login auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom
 import Empty from './View/Empty';
 import Home from './View/Home';
 import Login from './View/Login';
+import { AuthLogin } from './Common/Auth';
 import './App.scss';
 
 function App() {
@@ -13,6 +14,13 @@ function App() {
         return <Redirect to="/home"></Redirect>
       }}></Route>
       <Route path="/home" render={(props) => {
+        // 校验当前用户是否已经登录，如果没有登录，跳转到登录页面
+        if(!AuthLogin()) {
+          // 跳转到登录页面前，记录当前用户要请求的页面，登录成功之后会
+          // 跳转到用户之前要请求的页面。
+          return <Redirect to={`/login?preurl=${props.match.path}`}></Redirect>
+        }
+        // 权限校验。
         return <Home {...props}></Home>
       }} ></Route>
       <Route path="/login" component={Login}></Route>
