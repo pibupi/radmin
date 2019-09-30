32cd6228d190e89527d62a103bc790c47bbe9864
aicoder <malun666@126.com>
2019-09-30 20:23:52 +0800

    full screen layout

--- src/View/Home/index.js
+++ src/View/Home/index.js
@@ -1,17 +1,20 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import { Layout } from 'antd';
 
+const { Header, Footer, Sider, Content } = Layout;
 class Home extends Component {
   render () {
     return (
-      <div>
-          Home
-      </div>
+      <Layout style={{minHeight: '100vh'}}>
+        <Header style={{color: '#fff'}}>顶部</Header>
+        <Layout>
+          <Sider style={{backgroundColor: 'silver'}}>左侧</Sider>
+          <Content>内容</Content>
+        </Layout>
+        <Footer style={{backgroundColor: 'silver'}}>底部</Footer>
+      </Layout>
     )
   }
-}
-
-Home.propTypes = {
-
 }
 
 export default Home
