23fdd4198b74ebd1b9a420005d8983165e459617
aicoder <malun666@126.com>
2019-10-04 14:58:52 +0800

    added the table for the user mgr

--- src/View/Admin/UserMgr/index.js
+++ src/View/Admin/UserMgr/index.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react'
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Table } from 'antd';
 import { Link } from 'react-router-dom';
 class UserMgr extends Component {
+  state = {
+    userlist: [{
+      id: 1,
+      name: 'aicoder',
+      phone: '18922222'
+    }, {
+      id: 2,
+      name: 'aicoder2',
+      phone: '189222223333'
+    }, {
+      id: 3,
+      name: 'aicoder4',
+      phone: '18922222333'
+    }],
+    columns: [{
+      key: 'id',
+      title: '编号',
+      dataIndex: 'id'
+    }, {
+      key: 'name',
+      title: '姓名',
+      dataIndex: 'name'
+    }, {
+      key: 'phone',
+      title: '电话',
+      dataIndex: 'phone'
+    }]
+  }
+
+  userRowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      console.log(selectedRowKeys, selectedRows);
+    }
+  }
   render () {
     return (
       <div className="admin-usermgr">
@@ -14,6 +48,14 @@ class UserMgr extends Component {
           </Breadcrumb.Item>
         </Breadcrumb>
         <hr/>
+        <Table
+          bordered
+          style={{backgroundColor: '#FEFEFE'}}
+          dataSource={this.state.userlist}
+          columns={this.state.columns}
+          rowSelection={this.userRowSelection}
+          rowKey="id"
+        ></Table>
       </div>
     )
   }
