a0bc0a079fe3e652c310aa01587938475a0172d9
aicoder <malun666@126.com>
2019-10-11 17:42:20 +0800

    added the arr diff for the userrole

--- src/View/Admin/UserMgr/SetRole.js
+++ src/View/Admin/UserMgr/SetRole.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { Modal, Checkbox, Row, Col } from 'antd'
+import { Modal, Checkbox, Row, Col, message } from 'antd'
 import service from '../../../Service';
-
+import { formateDate2String } from '../../../Common/Helper';
 class SetRole extends Component {
   state = {
     allRoles: [],       // 所有的角色信息的数组
@@ -37,7 +37,44 @@ class SetRole extends Component {
     this.setState({allCheckedRole: checkedRoleArr});
   }
   handleSubmitSetRole = () => {
-    console.log(this.state.allCheckedRole);
+    // console.log(this.state.allCheckedRole);
+    let { allCheckedRole, userRoles } = this.state;
+    let promiseArr = [];
+    // 判断要添加的
+    // 最终的选中的role集合中不在原来的关联表中存在，就是添加的新关联。
+    allCheckedRole.forEach((role, index) => {
+      let addRoleIndex = userRoles.findIndex(userRole => userRole.roleId === role.id);
+      if(addRoleIndex < 0) {
+        // 添加关联
+        let p1 = service.addUserRole({
+          id: Date.now() + index,
+          del: 0,
+          subon: formateDate2String(new Date()),
+          roleId: role.id,
+          userId: this.props.data.id
+        });
+        promiseArr.push(p1);
+      }
+    });
+    // 判断要删除的
+    userRoles.forEach(userRole => {
+      let searchRoleIndex = allCheckedRole.findIndex(role => role.id === userRole.roleId);
+      if(searchRoleIndex < 0) {
+        // 进行删除的关系
+        let p2 = service.deleteUserRole(userRole.id);
+        promiseArr.push(p2);
+      }
+    });
+    Promise
+      .all(promiseArr)
+      .then(res => {
+        message.info('设置成功');
+        this.props.close();
+      })
+      .catch(err => {
+        message.error('设置失败！');
+        console.log('err :', err);
+      });
   }
   render () {
     return (
